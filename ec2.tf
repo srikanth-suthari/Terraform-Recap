44256bffe8f582f9e6678ea8a21bed47735fad01
srikanth-suthari <suthari.srikanth@outlook.com>
2026-08-20 20:01:14 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,9 @@
 resource "aws_instance" "terraform" {
     ami = "ami-0332d564d76dbd8d6"
     instance_type = "t3.micro"
+
+    tags = {
+        Name = "Linux"
+        terraform = "true"
+    }
 }
