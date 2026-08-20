50fc0dac0384f89b46d5de915885007934734cae
srikanth-suthari <suthari.srikanth@outlook.com>
2026-08-20 20:10:41 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -7,3 +7,26 @@ resource "aws_instance" "terraform" {
         terraform = "true"
     }
 }
+
+resource "aws_security_group" "allow_all" {
+
+    #Incoming traffic or inbound rules = ingress
+    ingress {
+        from_port = 0 #from port 0 to all ports
+        to_port = 0
+        protocol = "-1" # -1 means all protocols like tcp/udp/http/https etc
+        cidr_blocks = ["0.0.0.0/0"]     #from internet (all networks)
+    }
+
+    #Outgoing traffic or outbound rules = egress
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "My-SG"
+    }
+}
