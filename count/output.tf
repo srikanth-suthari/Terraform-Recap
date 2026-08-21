9601b906c9cd2ed7f5978915cef9b9a1f6c4a35e
srikanth-suthari <suthari.srikanth@outlook.com>
2026-08-21 16:15:11 +0530

    terraform

--- count/output.tf
+++ count/output.tf
@@ -1,3 +1,7 @@
-output "instances_output" {
-    value = "aws_instance.terraform"
+# output "instances_output" {
+#     value = aws_instance.terraform
+# }
+
+output "instances_ips" {
+    value = aws_instance.terraform.public_ip
 }
