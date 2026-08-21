590088d9e520af237c0d8b0d0c41c99c82a30981
srikanth-suthari <suthari.srikanth@outlook.com>
2026-08-21 10:39:47 +0530

    terraform

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "terraform" {
-    ami = var.environment == "dev" ? "t3.micro" : "t3.small"
-    # ami = var.environemt == "prod" ? "t3.large" : "t2.micro"
-    # instance_type = "t3.micro"
+    ami = var.ami_id
+    instance_type = var.environment == "dev" ? "t3.micro" : "t3.small"
     vpc_security_group_ids = [aws_security_group.allow_all.id]
     tags = {
         Name = "Linux"
--- conditions/variables.tf
+++ conditions/variables.tf
@@ -1,3 +1,8 @@
+variable "ami_id" {
+    type = string
+    default = "ami-0332d564d76dbd8d6"
+}
+
 variable "environment" {
     type = string
     default = "dev"
