2e24882289aa40c1b72177aad7e8588fb06fd199
srikanth-suthari <suthari.srikanth@outlook.com>
2026-08-21 08:28:59 +0530

    terraform

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -7,47 +7,44 @@ resource "aws_instance" "terraform" {
 
 resource "aws_security_group" "allow_all" {
 
-    name = "My-Security-Group"
+    name = var.sg_name
 
     #Incoming traffic or inbound rules = ingress
     ingress {
-        from_port = 0 #from port 0 to all ports
-        to_port = 0
-        protocol = "-1" # -1 means all protocols like tcp/udp/http/https etc
-        cidr_blocks = ["0.0.0.0/0"]     #from internet (all networks)
+        from_port = var.ingress_from_port #from port 0 to all ports
+        to_port = var.ingress_to_port
+        protocol = var.protocol # -1 means all protocols like tcp/udp/http/https etc
+        cidr_blocks = var.ingress_cidr     #from internet (all networks)
     }
 
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+    # ingress {
+    #     from_port   = 80
+    #     to_port     = 80
+    #     protocol    = "tcp"
+    #     cidr_blocks = ["0.0.0.0/0"]
+    # }
 
-    ingress {
-        from_port   = 443
-        to_port     = 443
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+    # ingress {
+    #     from_port   = 443
+    #     to_port     = 443
+    #     protocol    = "tcp"
+    #     cidr_blocks = ["0.0.0.0/0"]
+    # }
 
-    ingress {
-        from_port   = 8080
-        to_port     = 8080
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+    # ingress {
+    #     from_port   = 8080
+    #     to_port     = 8080
+    #     protocol    = "tcp"
+    #     cidr_blocks = ["0.0.0.0/0"]
+    # }
 
     #Outgoing traffic or outbound rules = egress
     egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+        from_port = var.egress_from_port
+        to_port = var.ingress_to_port
+        protocol = var.protocol
+        cidr_blocks = var.egress_cidr
     }
 
-    tags = {
-        Name = "My-SG"
-        terraform = "true"
-    }
+    tags = var.sg_tags
 }
