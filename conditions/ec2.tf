cb535aed67b646344c8d6ce61794f1dcf651d303
srikanth-suthari <suthari.srikanth@outlook.com>
2026-08-21 10:37:04 +0530

    terraform

---
+++ conditions/ec2.tf
@@ -0,0 +1,57 @@
+resource "aws_instance" "terraform" {
+    ami = var.environment == "dev" ? "t3.micro" : "t3.small"
+    ami = var.environemt == "prod" ? "t3.large" : "t2.micro"
+    # instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_all.id]
+    tags = {
+        Name = "Linux"
+        terraform = "true"
+    }
+}
+
+resource "aws_security_group" "allow_all" {
+
+    name = "My-Security-Group"
+
+    #Incoming traffic or inbound rules = ingress
+    ingress {
+        from_port = 0 #from port 0 to all ports
+        to_port = 0
+        protocol = "-1" # -1 means all protocols like tcp/udp/http/https etc
+        cidr_blocks = ["0.0.0.0/0"]     #from internet (all networks)
+    }
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    #Outgoing traffic or outbound rules = egress
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "My-SG"
+        terraform = "true"
+    }
+}
---
+++ conditions/variables.tf
@@ -0,0 +1,9 @@
+variable "environment" {
+    type = string
+    default = "dev"
+}
+
+variable "environment" {
+    type = string
+    default = "prod"
+}
