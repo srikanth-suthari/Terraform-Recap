68972b486b16980ad74f7410db7aca8978f0b811
srikanth-suthari <suthari.srikanth@outlook.com>
2026-08-21 15:59:44 +0530

    terraform

--- count/ec2.tf
+++ count/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "terraform" {
-    count = 4
+    count = 10
     ami = "ami-0332d564d76dbd8d6"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_all.id]
--- count/variables.tf
+++ count/variables.tf
@@ -7,3 +7,13 @@ variable "instance_name" {
     type = list
     default = ["mongodb", "mysql", "rabbitmq", "redis", "catalogue", "user", "cart", "shipping", "payment", "dispatch"]
 }
+
+variable "zone_id" {
+    type = string
+    default = "Z04492443PE4G96M3YA8"
+}
+
+variable "domain_name" {
+    type = string
+    default = "www.srikanth-suthari.me"
+}
