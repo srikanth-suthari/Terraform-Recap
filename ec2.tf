b656cad6604f4ce907abe098d2399f2f456230e7
srikanth-suthari <suthari.srikanth@outlook.com>
2026-08-21 07:00:42 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,5 @@
 resource "aws_instance" "terraform" {
-    # ami = "ami-0332d564d76dbd8d6"
-    ami = "default"
+    ami = "ami-0332d564d76dbd8d6"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_all.id]
     tags = {
