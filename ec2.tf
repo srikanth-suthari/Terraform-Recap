f9fc8ba95c0bf1f7a71fd0fc67dcd8b9abbec0ad
srikanth-suthari <suthari.srikanth@outlook.com>
2026-08-21 07:09:47 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -20,6 +20,27 @@ resource "aws_security_group" "allow_all" {
         cidr_blocks = ["0.0.0.0/0"]     #from internet (all networks)
     }
 
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     #Outgoing traffic or outbound rules = egress
     egress {
         from_port = 0
