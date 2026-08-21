a41eb9bbba004de359855b30bf6e714ab1913885
srikanth-suthari <suthari.srikanth@outlook.com>
2026-08-21 13:17:35 +0530

    terraform

--- count/ec2.tf
+++ count/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "terraform" {
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_all.id]
     tags = {
-        Name = "${"linux"}"[count.index]
+        Name = "${"linux"}".var.instance_name[count.index]
         terraform = "true"
     }
 }
