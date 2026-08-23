fe23394ee58a1d8da5b45dbe3f75489ba02fb04f
srikanth-suthari <suthari.srikanth@outlook.com>
2026-08-23 22:50:42 +0530

    terraform

--- for-loop/output.tf
+++ for-loop/output.tf
@@ -1,7 +1,7 @@
-output "instance_output" {
-    value = aws_instance.terraform.*.private_ip
-}
-
-# output "instances_private_ips" {
-#     value = aws_instance.terraform.*
+# output "instance_output" {
+#     value = aws_instance.terraform.*.private_ip
 # }
+
+output "instances_private_ips" {
+    value = aws_instance.terraform.*
+}
