f151dfebb403d92b5d345393e64462594613eed8
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-02 11:04:57 +0530

    terraform

--- count/variables.tf
+++ count/variables.tf
@@ -1,7 +1,7 @@
-# variable "ami_id" {
-#     type = string
-#     default = "ami-0220d79f3f480ecf5"
-# }
+variable "ami_id" {
+    type = string
+    default = "ami-0220d79f3f480ecf5"
+}
 
 variable "instance_name" {
     type = list
