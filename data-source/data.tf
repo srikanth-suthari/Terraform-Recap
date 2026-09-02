dcf6fc2bf1cc59060a13cd5cb831c61739e6f895
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-02 11:00:42 +0530

    terraform

--- data-source/data.tf
+++ data-source/data.tf
@@ -1,21 +1,31 @@
-#
+# data "aws_ami" "join-devops-ami" {
+#     owners = ["973714476881"]
+#     most_recent= true
 
-data "aws_ami" "join-devops-ami" {
-  owners      = ["973714476881"]
-  most_recent = true
+#     filter {
+#         name   = "name"
+#         values = ["RHEL-9-DevOps-Practice"]
+#     }
 
-  filter {
-    name   = "name"
-    values = ["RHEL-9-DevOps-Practice*"] # Added * wildcard
-  }
+#     filter {
+#         name   = "root-device-type"
+#         values = ["ebs"]
+#     }
 
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
+#     filter {
+#         name   = "virtualization-type"
+#         values = ["hvm"]
+#     }
+# }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+# output "ami_id" {
+#     value = data.aws_ami.join-devops-ami.id
+# }
+
+data "aws_instance" "terraform" {
+    instance_id = "i-0cec471f1d9c08c34"
+}
+
+output "mongodb_info" {
+    value = data.aws_instance.terraform.public_ip
 }
