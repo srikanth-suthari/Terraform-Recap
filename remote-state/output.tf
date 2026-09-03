248f96bcdad96185e4cf32638b56f8df3213efe3
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-03 09:26:32 +0530

    terraform

--- remote-state/output.tf
+++ remote-state/output.tf
@@ -1,7 +1,7 @@
-output "instance_output" {
+output "instance_private_ip" {
     value = resource.aws_instance.ec2_instance.private_ip
 }
 
-output "instance_output" {
+output "instance_id" {
     value = resource.aws_instance.ec2_instance.instance_id
 }
