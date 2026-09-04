fb700651c846b016eacd22e930816ece03ead166
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-04 13:51:33 +0530

    terraform

--- locals/main.tf
+++ locals/main.tf
@@ -1,6 +1,4 @@
 resource "aws_instance" "ec2_instance" {
-    # count = length(local.instances)
-    for_each = var.instances
     ami = "ami-0332d564d76dbd8d6"
     instance_type = "t3.micro"
     tags = local.ec2_tags
