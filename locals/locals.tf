45ce3bcdcd9596363064e29141a13ce8d2029b7f
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-04 13:46:09 +0530

    terraform

--- locals/locals.tf
+++ locals/locals.tf
@@ -3,7 +3,7 @@ locals {
   for_each = var.instances
   instance_type = "t3.micro"
   # Interpolation with variables is possible in locals
-  common_name = "${each.key}-${var.project}-${var.environment}" #instance_name-roboshop-dev
+  common_name = "${locals.each.key}-${var.project}-${var.environment}" #instance_name-roboshop-dev
   #common_name = "${var.project}-${var.environment}"   #roboshop-dev
   ec2_tags = merge(
     var.common_tags,
