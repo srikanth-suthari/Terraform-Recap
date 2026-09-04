5fa673212f2606974458dd4a6f115f397936f16f
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-04 13:19:13 +0530

    terraform

--- locals/locals.tf
+++ locals/locals.tf
@@ -1,11 +1,12 @@
 locals {
-    instance_type = "t3.micro"
-    # Interpolation with variables is possible in locals
-    common_name = "${var.project}-${var.environment}"   #roboshop-dev
-    ec2_tags = merge(
-      var.common_tags,
-      {
-        Name = "${local.common_name}-local-demo"
-      }
-    )
+  # instances = ["mongodb", "mysql", "user", "cart"]
+  instance_type = "t3.micro"
+  # Interpolation with variables is possible in locals
+  common_name = "${var.project}-${var.environment}"   #roboshop-dev
+  ec2_tags = merge(
+    var.common_tags,
+    {
+      Name = "${local.common_name}-local-demo"
+    }
+  )
 }
--- locals/main.tf
+++ locals/main.tf
@@ -1,4 +1,6 @@
 resource "aws_instance" "ec2_instance" {
+    # count = length(local.instances)
     ami = "ami-0332d564d76dbd8d6"
     instance_type = "t3.micro"
+    tags = local.ec2_tags
 }
