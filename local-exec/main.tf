85cbb78e5828a056e892cbf176e010f60b634efc
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-05 21:02:12 +0530

    terraform

---
+++ local-exec/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "ec2_instance" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    tags = {
+        Name = "local-exec-demo"
+    }
+
+    provisioner "local-exec" {
+        command = "echo ${self.*} > local-exec.txt "   #the commands should be in double quotes
+        on_failure = contiue
+    }
+}
